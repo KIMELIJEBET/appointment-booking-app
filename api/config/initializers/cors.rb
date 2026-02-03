188048bd4604952f5598a4b2c348ed069a938d9c
KIMELIJEBET <dorothykimeli@gmail.com>
2026-02-03 14:00:28 +0300

    updating cors.rb

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # React app URL
+    origins 'https://appointment-booking-app-zley.onrender.com'  # React app URL
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
