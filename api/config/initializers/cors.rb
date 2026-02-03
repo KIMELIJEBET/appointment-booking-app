bfc50315e59f334f0bbac02185e88ba150be061d
KIMELIJEBET <dorothykimeli@gmail.com>
2026-02-03 11:40:29 +0300

    fetching data

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'  # React app URL
+    origins 'http://localhost:3001'  # React app URL
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
