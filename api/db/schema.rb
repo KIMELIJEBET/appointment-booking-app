89f9f2cf3bd4bd77eced32fb0aa66c1285d5c155
KIMELIJEBET <dorothykimeli@gmail.com>
2026-02-03 13:36:19 +0300

    deploying

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_192127) do
+ActiveRecord::Schema[7.0].define(version: 2026_02_02_192127) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "created_at", null: false
